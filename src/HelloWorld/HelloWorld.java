092f2b63da1cf4df1d790ec61b3e7c8fc2f00eef
unknown <caueslopes@gmail.com>
2025-05-20 23:42:32 -0300

    Projeto HelloWorld Finalizado

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		if (args.length == 0)
+			System.out.println("Use: java helloWorld.HelloWorld \"Cauê Schuch Lopes\"");
+		System.out.println("Olá " + args[0]);
+		
+
+	}
+
+}
